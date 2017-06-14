1e2057ae7bde52b2ee5f6e261c03a431a99af6f9
wagnert <t.wagner@techdivision.com>
2017-06-14 15:00:35 +0200

    Use Robo for Travis-CI build process + Refactoring for new ConnectionInterface + SqlStatementsInterface

--- src/Actions/Processors/ProductLinkCreateProcessor.php
+++ src/Actions/Processors/ProductLinkCreateProcessor.php
@@ -48,7 +48,7 @@ class ProductLinkCreateProcessor extends AbstractCreateProcessor
 
         // return the array with the SQL statements that has to be prepared
         return array(
-            $utilityClassName::CREATE_PRODUCT_LINK => $utilityClassName::CREATE_PRODUCT_LINK
+            $utilityClassName::CREATE_PRODUCT_LINK => $this->getUtilityClass()->find($utilityClassName::CREATE_PRODUCT_LINK)
         );
     }
 
