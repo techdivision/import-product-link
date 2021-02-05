9ef930d5cd2c185af458c530107ea0894fe7495b
Tim Wagner <t.wagner@techdivision.com>
2021-02-05 10:35:11 +0100

    Add @deprecated annotations for upcoming 25.x branch

--- src/Actions/Processors/ProductLinkAttributeDecimalCreateProcessor.php
+++ src/Actions/Processors/ProductLinkAttributeDecimalCreateProcessor.php
@@ -26,11 +26,13 @@ use TechDivision\Import\Actions\Processors\AbstractCreateProcessor;
 /**
  * The product link attribute decimal create processor implementation.
  *
- * @author    Tim Wagner <t.wagner@techdivision.com>
- * @copyright 2019 TechDivision GmbH <info@techdivision.com>
- * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
- * @link      https://github.com/techdivision/import-product-link
- * @link      http://www.techdivision.com
+ * @author     Tim Wagner <t.wagner@techdivision.com>
+ * @copyright  2021 TechDivision GmbH <info@techdivision.com>
+ * @license    http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link       https://github.com/techdivision/import-product-link
+ * @link       http://www.techdivision.com
+ * @deprecated Since 24.0.0
+ * @see        \TechDivision\Import\Actions\Processors\GenericProcessor
  */
 class ProductLinkAttributeDecimalCreateProcessor extends AbstractCreateProcessor
 {
